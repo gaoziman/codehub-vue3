fa1932b99260f555aa0ff2f04822167ca4468b8d
gaoziman <2942894660@qq.com>
2024-07-24 11:13:19 +0800

    v0.1.0 feat/导航栏组件实现

--- src/layouts/admin/components/AdminTagList.vue
+++ src/layouts/admin/components/AdminTagList.vue
@@ -1,14 +1,140 @@
 <template>
-  <div class="bg-indigo-700 text-white">
-    标签导航栏
+  <!-- 左边：标签导航栏 -->
+  <div class="fixed top-[64px] h-[44px] px-2 right-0 z-50 flex items-center bg-white" style="left: 250px;" :style="{left: menuStore.menuWidth}">
+    <el-tabs v-model="editableTabsValue" type="card" class="demo-tabs" closable @tab-remove="removeTab"  style="min-width: 10px;">
+      <el-tab-pane v-for="item in editableTabs" :key="item.name" :label="item.title" :name="item.name">
+
+      </el-tab-pane>
+    </el-tabs>
+
+
+    <!-- 右侧下拉菜单 -->
+    <span class="ml-auto flex items-center justify-center h-[32px] w-[32px]">
+            <el-dropdown>
+                <span class="el-dropdown-link">
+                    <el-icon>
+                        <arrow-down />
+                    </el-icon>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>关闭其他</el-dropdown-item>
+                        <el-dropdown-item>关闭全部</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </span>
   </div>
+  <div class="h-[44px]"></div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import {ArrowDown} from "@element-plus/icons-vue";
+import { useMenuStore } from '@/stores/menu'
 
+const menuStore = useMenuStore()
+
+let tabIndex = 2
+const editableTabsValue = ref('2')
+const editableTabs = ref([
+  {
+    title: 'Tab 1',
+    name: '1',
+    content: 'Tab 1 content',
+  },
+  {
+    title: 'Tab 2',
+    name: '2',
+    content: 'Tab 2 content',
+  },
+])
+
+const addTab = (targetName) => {
+  const newTabName = `${++tabIndex}`
+  editableTabs.value.push({
+    title: 'New Tab',
+    name: newTabName,
+    content: 'New Tab content',
+  })
+  editableTabsValue.value = newTabName
+}
+const removeTab = (targetName) => {
+  const tabs = editableTabs.value
+  let activeName = editableTabsValue.value
+  if (activeName === targetName) {
+    tabs.forEach((tab, index) => {
+      if (tab.name === targetName) {
+        const nextTab = tabs[index + 1] || tabs[index - 1]
+        if (nextTab) {
+          activeName = nextTab.name
+        }
+      }
+    })
+  }
+
+  editableTabsValue.value = activeName
+  editableTabs.value = tabs.filter((tab) => tab.name !== targetName)
+}
 </script>
 
+<style>
+.el-tabs__item {
+  font-size: 12px;
+  border: 1px solid #d8dce5!important;
+  border-radius: 3px!important;
+}
 
-<style scoped>
+.el-tabs--card>.el-tabs__header .el-tabs__item {
+  margin-left: 0.1rem!important;
+  margin-right: 0.1rem!important;
+}
 
+.el-tabs__item.is-active {
+  background-color: var(--el-color-primary)!important;
+  color: #fff;
+}
+
+.el-tabs__item.is-active::before {
+  content: "";
+  background-color: #fff;
+  display: inline-block;
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  position: relative;
+  margin-right: 4px;
+}
+
+.el-tabs {
+  height: 32px;
+}
+
+.el-tabs__header {
+  margin-bottom: 0;
+}
+
+.el-tabs--card>.el-tabs__header .el-tabs__nav {
+  border: 0;
+}
+
+.el-tabs--card>.el-tabs__header .el-tabs__item {
+  height: 32px;
+  line-height: 32px;
+  border: 0;
+  background: #fff;
+}
+
+.el-tabs--card>.el-tabs__header {
+  border: 0;
+}
+
+.el-tabs__nav-prev, .el-tabs__nav-next {
+  line-height: 35px;
+}
+
+.is-disabled {
+  cursor: not-allowed;
+  color: #d1d5db;
+}
 </style>
