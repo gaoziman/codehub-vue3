048f8ce20021b711a6e20994f4a7ef2a55aa33c3
gaoziman <2942894660@qq.com>
2024-07-26 11:31:44 +0800

    v0.1.0 feat/实现组件中文化

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <router-view/>
+  <!-- 设置语言为中文 -->
+  <el-config-provider :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
 
 
 <script setup>
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+const locale = zhCn
 </script>
 
 
